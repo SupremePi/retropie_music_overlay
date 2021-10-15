a9cc28f0c274709f1e84fb93eceef1a5f40420a3
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2021-10-15 15:41:50 -0500

    Update BGM_Install.sh

--- BGM_Install.sh
+++ BGM_Install.sh
@@ -30,7 +30,7 @@ if [[ $currentuser == "root" ]]; then
 fi
 
 ##### Download the files needed and install the script + utilities
-git clone -b OGST-Beta https://github.com/madmodder123/retropie_music_overlay.git
+git clone -b OGST-Beta https://github.com/ALLRiPPED/retropie_music_overlay.git
 cd ~/retropie_music_overlay
 if [[ $currentuser == "pi" ]]; then #Use pngview if using Raspberry Pi
 	sudo chmod +x pngview
