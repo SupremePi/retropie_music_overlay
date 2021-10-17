797249a223705f247496e0e34f1daf6bd7bd6f91
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2021-10-17 04:55:13 -0500

    Update BGM_Install.sh

--- BGM_Install.sh
+++ BGM_Install.sh
@@ -79,7 +79,7 @@ sudo chown $currentuser:$currentuser backgroundmusic.sh
 sudo chown $currentuser:$currentuser custombgmoptions.sh
 sudo chmod 0777 backgroundmusic.sh
 sudo chmod 0777 custombgmoptions.sh
-CONTENT1="	<game>\n	<path>./audiotools</path>\n		<name>Audio Tools</name>\n		<desc>Audio Tools and More Options.</desc>\n		<image>./icons/audiosettings.png</image>\n	</game>"
+CONTENT1="	<game>\n<path>./audiotools</path>\n<name>Audio Tools</name>\n<desc>Audio Tools and More Options.</desc>\n<image>./icons/audiosettings.png</image>\n</game>"
 C1=$(echo $CONTENT1 | sed 's/\//\\\//g')
 if [ ! -d  "~/RetroPie/retropiemenu/audiotools/" ];
 then
@@ -116,8 +116,8 @@ then
 	cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ~/RetroPie/retropiemenu/gamelist.xml
 fi
 
-CONTENT2="	<game>\n	<path>./audiotools/backgroundmusic.sh</path>\n		<name>Background Music</name>\n		<desc>Toggles background music options such as music ON/OFF and volume control.</desc>\n		<image>./icons/backgroundmusic.png</image>\n	</game>"
-C2=$(echo $CONTENT1 | sed 's/\//\\\//g')
+CONTENT2="<game>\n<path>./audiotools/backgroundmusic.sh</path>\n<name>Background Music</name>\n<desc>Toggles background music options such as music ON/OFF and volume control.</desc>\n<image>./icons/backgroundmusic.png</image>\n</game>"
+C2=$(echo $CONTENT2 | sed 's/\//\\\//g')
 if grep -q backgroundmusic.sh "/home/$currentuser/RetroPie/retropiemenu/gamelist.xml"; then # Check if menu entry is already there or not
 	echo "gamelist.xml entry confirmed"
 else
@@ -125,8 +125,8 @@ else
 	cat ~/temp > ~/RetroPie/retropiemenu/gamelist.xml
 	rm -f ~/temp
 fi
-CONTENT3="	<game>\n		<path>./audiotools/custombgmoptions.sh</path>\n		<name>Background Music Options</name>\n		<desc>A background music script to set and play MP3/OGG files during menu navigation in both Emulation Station and Attract Mode. A Few special new folders have been created in the /roms directory called "music", and subfolders from there named "arcade", "bttf", "st", &amp; this last one "custom" is for placing your MP3 files into. Once you place your music files into this folder and enable it, the music will automatically begin playing.</desc>\n		<image>./icons/backgroundmusic.png</image>\n	</game>"
-C3=$(echo $CONTENT1 | sed 's/\//\\\//g')
+CONTENT3="<game>\n<path>./audiotools/custombgmoptions.sh</path>\n<name>Background Music Options</name>\n<desc>A background music script to set and play MP3/OGG files during menu navigation in both Emulation Station and Attract Mode. A Few special new folders have been created in the /roms directory called "music", and subfolders from there named "arcade", "bttf", "st", &amp; this last one "custom" is for placing your MP3 files into. Once you place your music files into this folder and enable it, the music will automatically begin playing.</desc>\n<image>./icons/backgroundmusic.png</image>\n</game>"
+C3=$(echo $CONTENT3 | sed 's/\//\\\//g')
 if grep -q custombgmoptions.sh "/home/$currentuser/RetroPie/retropiemenu/gamelist.xml"; then # Check if menu entry is already there or not
 	echo "gamelist.xml entry confirmed"
 else
