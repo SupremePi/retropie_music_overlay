993e8184f0f923a57287f93089649006f89414a6
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2021-10-27 21:25:30 -0500

    another small update

--- BGM.py
+++ BGM.py
@@ -12,21 +12,21 @@ import subprocess # used to grab screen resolution
 
 ###CONFIG SECTION###
 startdelay = 0 # Value (in seconds) to delay audio start.  If you have a splash screen with audio and the script is playing music over the top of it, increase this value to delay the script from starting.
-musicdirac = '/home/pi/RetroPie/roms/music/arcade'
-musicdirbttf = '/home/pi/RetroPie/roms/music/bttf'
-musicdircustom = '/home/pi/RetroPie/roms/music/custom'
-musicdirnt = '/home/pi/RetroPie/roms/music/nt'
-musicdirst = '/home/pi/RetroPie/roms/music/st'
-musicdiruvf = '/home/pi/RetroPie/roms/music/uvf'
-musicdirvenom = '/home/pi/RetroPie/roms/music/venom'
-musicdiroff = '/home/pi/RetroPie/roms/music'
-musicdir = musicdiroff # "~/" is the equivalent to "/home/pi"
+musicdirac = '~/RetroPie/roms/music/arcade'
+musicdirbttf = '~/RetroPie/roms/music/bttf'
+musicdircustom = '~/RetroPie/roms/music/custom'
+musicdirnt = '~/RetroPie/roms/music/nt'
+musicdirst = '~/RetroPie/roms/music/st'
+musicdiruvf = '~/RetroPie/roms/music/uvf'
+musicdirvenom = '~/RetroPie/roms/music/venom'
+musicdiroff = '~/RetroPie/roms/music'
+musicdir = musicdiroff # "~/" is the equivalent to "~"
 maxvolume = 0.50
 volumefadespeed = 0.02
 restart = True # If true, this will cause the script to fade the music out and -stop- the song rather than pause it.
 startsong = "" # if this is not blank, this is the EXACT, CaSeSeNsAtIvE filename of the song you always want to play first on boot.
 
-### if ~ is used, change it to home directory (EXAMPLE: "~/BGM" to "/home/pi/BGM")
+### if ~ is used, change it to home directory (EXAMPLE: "~/BGM" to "~/BGM")
 if "~/" in musicdir:
 	musicdir = os.path.expanduser(musicdir)
 
@@ -193,11 +193,11 @@ while True:
 				continue
 				
 	#Check to see if the DisableMusic file exists; if it does, stop doing everything!
-	if os.path.exists('/home/pi/RetroPie/roms/music/DisableMusic'):
+	if os.path.exists('~/RetroPie/roms/music/DisableMusic'):
 		print "DisableMusic found!"
 		if mixer.music.get_busy():
 			mixer.music.stop();
-		while (os.path.exists('/home/pi/RetroPie/roms/music/DisableMusic')):
+		while (os.path.exists('~/RetroPie/roms/music/DisableMusic')):
 			time.sleep(15)
 		print "DisableMusic gone!"
 
